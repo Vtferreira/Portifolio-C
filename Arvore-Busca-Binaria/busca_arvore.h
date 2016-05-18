ded1b7cefb290ee4d2b3dbd75ca772901d25470f
Vinicius Tarcisio Ferreira <vinicius.tarcisio666@gmail.com>
2016-05-17 22:19:07 -0300

    Add files via upload

---
+++ Arvore-Busca-Binaria/busca_arvore.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node* Node;
+    struct node{
+           int chave;
+           Node *esqueda;
+           Node *direita;       
+           };
+        
+int busca_arvore(*pt, x, f){
+    
+                      
+      
+      }
+  
+  
+  
+  
+
+
---
+++ Arvore-Busca-Binaria/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "busca_arvore.h"
+
+int main(int argc, char *argv[])
+{
+  int pt, x, f;
+  
+  
+  
+  
+  system("PAUSE");	
+  return 0;
+}
